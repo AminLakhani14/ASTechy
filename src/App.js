2f3eac7ae6eec55ef379dfee0f1de0703548d46e
Syed Faiq Ali Jafri <faiqalisyed00@gmail.com>
2024-03-01 04:03:22 +0500

    FaiqPrototype: Added Footer and worked in navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./BasePage";
 import Navbar from "./Component/NavBar/NavBar";
+import Footer from "./Component/Footer/Footer";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <RouterProvider router={router} />
+      <Footer />
     </div>
   );
 }
