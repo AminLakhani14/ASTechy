6ba28d8e35814da96c0f45c799e1d95a9657bae9
Ghufran Ali <ghufranali510@gmail.com>
2024-03-09 21:36:48 +0500

    GhufranPrototype: Fixed Navbar Issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./BasePage";
-import Navbar from "./Component/NavBar/NavBar";
+import Navbar from "./Component/NavBar/Navbar";
 import Service from "./Component/Home/Services/Service";
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
       <Service />
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
     </div>
   );
 }
