9d1194e45136a439514abb5bfbaa55852a7d0a97
Amin Lakhani <alakhani@macrosoftinc.com>
2024-03-13 14:40:41 +0500

    AminBranch: responsive work on work screens

--- src/Global/LeftSideExploreMore.jsx
+++ src/Global/LeftSideExploreMore.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import "../CSS/style.css"
 
 function LeftSideExploreMore(props) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    // Function to update the windowWidth state when the resize event occurs
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Attach the event listener
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
     return (
-        <div className="row" style={{marginTop:"90px"}}>
+        <div className={windowWidth < 500 ? "d-flex" : "row"}
+        style={
+          windowWidth <= 500
+            ? { marginTop:"90px", flexDirection: "column-reverse"  }
+            : { marginTop:"90px"}
+        }>
         <div className="col-sm-1"></div>
 
         <div className="col-sm-5 my-5 leftsideExplore">
