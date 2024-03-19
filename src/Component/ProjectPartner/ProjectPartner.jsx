0ebb935e39cf7ecbc8037da20ed206621d9ca52e
Syed Faiq Ali Jafri <faiqalisyed00@gmail.com>
2024-03-19 07:01:37 +0500

    FaiqPrototype: UI work

--- src/Component/ProjectPartner/ProjectPartner.jsx
+++ src/Component/ProjectPartner/ProjectPartner.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ProjectPartner.css';
+import experience from '../Assets/icons8-customer-insight-48.png';
+import talented from '../Assets/icons8-multiplayer-64.png';
+import webdev from '../Assets/icons8-website-50.png';
+import projectdel from '../Assets/icons8-project-50.png';
+import countries from '../Assets/icons8-middle-east-48.png';
+import satisfaction from '../Assets/icons8-satisfaction-50.png';
 
 const ProjectPartner = ({ mainText, subText, buttonText }) => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
     return (
-        <div className="container">
-            <div className="left-side">
+        <div className={windowWidth < 980 ? "ppsmallcontainer" : "ppcontainer"}>
+            <div className="ppleft-side">
                 <h2 className='partnerheading'>Why Choose Ropstam as Your <span className='changecolor'>{mainText} Company</span></h2>
                 <p className='partnerpara'>{subText}</p>
                 <h3 className='togetherpara'>Let’s Start a New {buttonText} Project Together</h3>
@@ -12,31 +30,31 @@ const ProjectPartner = ({ mainText, subText, buttonText }) => {
                     <button className="contactUsButton"><b>Contact Us</b></button>
                 </a>
             </div>
-            <div className="right-side">
-                <div className="card-container">
+            <div className="ppright-side">
+                <div className="ppcard-container">
                     <div style={{ display: 'flex' }}>
-                        <div className="card">
+                        <div className="ppcard">
                             <div className='subcontentcard'>
-                                <img src="image1.jpg" alt="Image 1" />
-                                <div className="content">
+                                <img src={experience} alt="Image 1" />
+                                <div className="ppcontent">
                                     <h4 className="row1">14+</h4>
                                     <p className="row">Year of Experience</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="ppcard">
                             <div className='subcontentcard'>
-                                <img src="image2.jpg" alt="Image 2" />
-                                <div className="content">
+                                <img src={talented} alt="Image 2" />
+                                <div className="ppcontent">
                                     <h4 className="row1">100+</h4>
                                     <p className="row">Talented Squad</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="ppcard">
                             <div className='subcontentcard'>
-                                <img src="image3.jpg" alt="Image 3" />
-                                <div className="content">
+                                <img src={projectdel} alt="Image 3" />
+                                <div className="ppcontent">
                                     <h4 className="row1">100+</h4>
                                     <p className="row">Projects Delivered</p>
                                 </div>
@@ -44,28 +62,28 @@ const ProjectPartner = ({ mainText, subText, buttonText }) => {
                         </div>
                     </div>
                     <div style={{ display: 'flex' }}>
-                        <div className="card">
+                        <div className="ppcard">
                             <div className='subcontentcard'>
-                                <img src="image4.jpg" alt="Image 4" />
-                                <div className="content">
+                                <img src={webdev} alt="Image 4" />
+                                <div className="ppcontent">
                                     <h4 className="row1">300+</h4>
                                     <p className="row">Designs Build</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="ppcard">
                             <div className='subcontentcard'>
-                                <img src="image5.jpg" alt="Image 5" />
-                                <div className="content">
+                                <img src={countries} alt="Image 5" />
+                                <div className="ppcontent">
                                     <h4 className="row1">30+</h4>
                                     <p className="row">Countries Served</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="card">
+                        <div className="ppcard">
                             <div className='subcontentcard'>
-                                <img src="image6.jpg" alt="Image 6" />
-                                <div className="content">
+                                <img src={satisfaction} alt="Image 6" />
+                                <div className="ppcontent">
                                     <h4 className="row1">100%</h4>
                                     <p className="row">Client Satisfaction</p>
                                 </div>
